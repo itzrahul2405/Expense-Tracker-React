9d7abe92a3cd71661baab6a0fdd59b5a839658e3
RahulSharma <rahulmlc2003@gmail.com>
2023-12-25 17:31:38 +0530

    email verification

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const HomePage = () => {
+
+  const verifyEmailHandler = async() => {
+    try{
+      const resp = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAuVAC3jcFhmukTcZotAQrrHnfVzSuM0O4',{
+        method: 'POST',
+        body: JSON.stringify({requestType: "VERIFY_EMAIL", idToken: localStorage.getItem('token')}),
+        headers: {'Content-Type': 'application/json'}
+      })
+      if(!resp.ok){
+        throw new Error('Email Verification Failed!')
+      }
+      const data = await resp.json()
+      console.log(data)
+    }
+    catch(error){
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className={classes.container}>
       <div className={classes.upper_container}>
@@ -12,6 +31,9 @@ const HomePage = () => {
         </p>
       </div>
         <hr />
+      <div>
+        <button onClick={verifyEmailHandler}>Verify Email</button>
+      </div>
     </div>
   );
 };
